b3f52bdf4ee038a766341958ceec507f4307a124
Mehul Raj Ranjan <164037510+mehulrajranjan@users.noreply.github.com>
2026-02-12 21:34:46 +0530

    Added solution - LeetHub

---
+++ 700975-intro-to-dictionary-python-GFG/700975-intro-to-dictionary-python.py
@@ -0,0 +1,14 @@
+# Function to create dictionary
+# arr is list of tuple. tuple contain name and marks.
+
+
+def create_dict(arr):
+
+    dict = {}
+
+    # Your code here
+    # Hint: use loop to iterate through arr
+    # and assign key value to the dict
+    for name,marks in arr:
+        dict[name] = marks
+    return dict
